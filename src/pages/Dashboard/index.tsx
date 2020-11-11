8c40d4a70ea50edc1140b4d9cb38b17969db444c
Rafael Gomes <rafaell.gomes@gmail.com>
2020-11-10 22:27:22 -0300

    Dashboard styling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/742710?s=460&v=4"
+            alt="Rafael Gomes"
+          />
+          <div>
+            <strong>gobarber_backend</strong>
+            <p>Node API in development during bootcamp for GoBarber</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/742710?s=460&v=4"
+            alt="Rafael Gomes"
+          />
+          <div>
+            <strong>gobarber_backend</strong>
+            <p>Node API in development during bootcamp for GoBarber</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/742710?s=460&v=4"
+            alt="Rafael Gomes"
+          />
+          <div>
+            <strong>gobarber_backend</strong>
+            <p>Node API in development during bootcamp for GoBarber</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
